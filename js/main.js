f4516ee27e1371f391cd6a1b4ba79ce625f974bf
blackbad1 <fawcetteugene@gmail.com>
2019-08-10 22:05:47 +0300

    Scroll Button.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
 var ShoppingCart = (function($) {
   "use strict";
 
